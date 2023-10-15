d08d4dce5951a75c4f955e3d24d1efc710827645
marambelhajrhouma <140088088+marambelhajrhouma@users.noreply.github.com>
2023-10-15 19:39:29 +0100

    Add files

--- Test.java
+++ Test.java
@@ -1,28 +1,53 @@
-package TP4;
+package EX1;
+import java.util.Scanner;
 
 public class Test {
-	public static void main(String args[]) {
-		AgenceMobiliere ag=new AgenceMobiliere(5);
-		
-		//
-		Vehicule v=new Vehicule("clio","classique",20.000,2000);
-		Vehicule w=new Vehicule("Mercedes","Fantôme",60.000,2008);
-		Vehicule x=new Vehicule("kia","picanto",35.000,2012);
-		
-		ag.ajoutVehicule(v);
-		ag.ajoutVehicule(w);
-		ag.ajoutVehicule(x);
-		
-		System.out.println("affichage:");
-		ag.decrisAg();
-		
-		System.out.println("\n la marque: \n");
-		ag.selection("Mercedes");
-		
-		System.out.println("\n prix \n");
-		ag.selection(40.000);
-		
-		System.out.println("\n ancienne \n");
-		ag.ancienne();
-	}
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        int nb;
+
+	        do {
+	            System.out.println("Entrer une val: ");
+	            nb = scanner.nextInt();
+	        } while (nb < 0 || nb > Tab.getNmax());
+
+	        // Remplissage
+	        float[] noteCtrl = new float[nb];
+	        Tab.remplir(noteCtrl, scanner);
+	        System.out.println("Remplir ctrl ");
+
+	        float[] noteExam = new float[nb];
+	        Tab.remplir(noteExam, scanner);
+	        System.out.println("Remplir exam ");
+
+	        // Affichage
+	        System.out.println("Affichage ctrl");
+	        Tab.lister(noteCtrl);
+	        System.out.println("Affichage exam");
+	        Tab.lister(noteExam);
+
+	        // Calcul de la moyenne
+	        float[] moyennes = Tab.moyenne(noteCtrl, noteExam);
+	        System.out.println("Moyennes des étudiants:");
+	        Tab.lister(moyennes);
+
+	        // Ajout d'un bonus
+	        float bonus = 1.5f;
+	        noteCtrl = Tab.additionner(bonus, noteCtrl);
+	        noteExam = Tab.additionner(bonus, noteExam);
+
+	        // Affichage avec bonus
+	        System.out.println("Affichage ctrl avec bonus");
+	        Tab.lister(noteCtrl);
+	        System.out.println("Affichage exam avec bonus");
+	        Tab.lister(noteExam);
+
+	        // Calcul de la moyenne avec bonus
+	        float[] moyennesAvecBonus = Tab.moyenne(noteCtrl, noteExam);
+	        System.out.println("Moyennes des étudiants avec bonus:");
+	        Tab.lister(moyennesAvecBonus);
+
+	        // Fermeture du scanner
+	        scanner.close();
+	    }
 }
